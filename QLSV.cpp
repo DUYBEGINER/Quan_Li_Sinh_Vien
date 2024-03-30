6a226fbacd0b4ba58e67a545712d620d0221ec66
DUYBEGINER <phucduygg8485@gmail.com>
2024-03-31 01:05:04 +0700

    first

---
+++ QLSV.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<iomanip>
+#include<cmath>
+#include<string>
+
+using namespace std;
+
+typedef struct SinhVien{
+    string HoTen[50];
+    string maSV[10];
+    int diemToan;
+    int diemVan;
+    int diemAnh;
+    int diemTB;
+} SV;
+
+
+int main(){
+
+    return 0;
+
+}
